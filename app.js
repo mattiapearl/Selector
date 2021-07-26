6d4901006198e3dae84cbc5cd5a0134ce1b75f02
Mattia <mattiapearl@gmail.com>
2021-07-26 14:27:36 +0200

    Browse feature added

--- app.js
+++ app.js
@@ -1,231 +1,249 @@
 // Pull list in similar form. If one of them is not defined, they won't show in the search.
-const listOfExternalWallets = [
-  {
-    "name": "calxes",
-    "type": "equity"
-  },
-  {
-    "name": "penicillation",
-    "type": "crypto"
-  },
-  {
-    "name": "downhaul",
-    "type": "equity"
-  },
-  {
-    "name": "unscrutable",
-    "type": "crypto"
-  },
-  {
-    "name": "nonmonarchial",
-    "type": "crypto"
-  },
-  {
-    "name": "sapropelic",
-    "type": "equity"
-  },
-  {
-    "name": "heartthrob",
-    "type": "equity"
-  },
-  {
-    "name": "aff",
-    "type": "equity"
-  },
-  {
-    "name": "subaudibleness",
-    "type": "equity"
-  },
-  {
-    "name": "holophote",
-    "type": "crypto"
-  },
-  {
-    "name": "deplored",
-    "type": "equity"
-  },
-  {
-    "name": "countrywide",
-    "type": "crypto"
-  },
-  {
-    "name": "disutility",
-    "type": "crypto"
-  },
-  {
-    "name": "barih",
-    "type": "equity"
-  },
-  {
-    "name": "opiated",
-    "type": "crypto"
-  },
-  {
-    "name": "unquotable",
-    "type": "crypto"
-  },
-  {
-    "name": "overliberalize",
-    "type": "crypto"
-  },
-  {
-    "name": "turion",
-    "type": "crypto"
-  },
-  {
-    "name": "chickenshit",
-    "type": "equity"
-  },
-  {
-    "name": "technic",
-    "type": "crypto"
-  },
-  {
-    "name": "subcancellous",
-    "type": "equity"
-  },
-  {
-    "name": "misauthorize",
-    "type": "crypto"
-  },
-  {
-    "name": "tendencious",
-    "type": "crypto"
-  },
-  {
-    "name": "celestite",
-    "type": "equity"
-  },
-  {
-    "name": "ameerate",
-    "type": "crypto"
-  },
-  {
-    "name": "dustless",
-    "type": "equity"
-  },
-  {
-    "name": "horseradish",
-    "type": "equity"
-  },
-  {
-    "name": "whips",
-    "type": "equity"
-  },
-  {
-    "name": "methylene",
-    "type": "crypto"
-  },
-  {
-    "name": "subpatellar",
-    "type": "crypto"
-  },
-  {
-    "name": "intolerableness",
-    "type": "crypto"
-  },
-  {
-    "name": "unterwalden",
-    "type": "crypto"
-  },
-  {
-    "name": "mastery",
-    "type": "equity"
-  },
-  {
-    "name": "giver",
-    "type": "crypto"
-  },
-  {
-    "name": "unsplit",
-    "type": "equity"
-  },
-  {
-    "name": "anticonservative",
-    "type": "equity"
-  },
-  {
-    "name": "feod",
-    "type": "equity"
-  },
-  {
-    "name": "hemoglobic",
-    "type": "equity"
-  },
-  {
-    "name": "ungraspable",
-    "type": "crypto"
-  },
-  {
-    "name": "hiver",
-    "type": "crypto"
-  },
-  {
-    "name": "recombine",
-    "type": "crypto"
-  },
-  {
-    "name": "anyplace",
-    "type": "equity"
-  },
-  {
-    "name": "unvindictive",
-    "type": "equity"
-  },
-  {
-    "name": "ontogenesis",
-    "type": "crypto"
-  },
-  {
-    "name": "chapleted",
-    "type": "equity"
-  },
-  {
-    "name": "rhizopus",
-    "type": "equity"
-  },
-  {
-    "name": "trochilus",
-    "type": "crypto"
-  },
-  {
-    "name": "quintal",
-    "type": "equity"
-  },
-  {
-    "name": "croupy",
-    "type": "crypto"
-  }
-];
-const listOfInternalWallets = [{
-  "name": "calxes",
-  "type": "equity"
-}]; 
+
 
 // Declarations
+let listOfExternalWallets = "";
+let listOfInternalWallets = "";
+
+//Function that assigns value to these two elements
+listOfExternalWallets = [
+  {
+    name: "calxes",
+    type: "equity",
+  },
+  {
+    name: "penicillation",
+    type: "crypto",
+  },
+  {
+    name: "downhaul",
+    type: "equity",
+  },
+  {
+    name: "unscrutable",
+    type: "crypto",
+  },
+  {
+    name: "nonmonarchial",
+    type: "crypto",
+  },
+  {
+    name: "sapropelic",
+    type: "equity",
+  },
+  {
+    name: "heartthrob",
+    type: "equity",
+  },
+  {
+    name: "aff",
+    type: "equity",
+  },
+  {
+    name: "subaudibleness",
+    type: "equity",
+  },
+  {
+    name: "holophote",
+    type: "crypto",
+  },
+  {
+    name: "deplored",
+    type: "equity",
+  },
+  {
+    name: "countrywide",
+    type: "crypto",
+  },
+  {
+    name: "disutility",
+    type: "crypto",
+  },
+  {
+    name: "barih",
+    type: "equity",
+  },
+  {
+    name: "opiated",
+    type: "crypto",
+  },
+  {
+    name: "unquotable",
+    type: "crypto",
+  },
+  {
+    name: "overliberalize",
+    type: "crypto",
+  },
+  {
+    name: "turion",
+    type: "crypto",
+  },
+  {
+    name: "chickenshit",
+    type: "equity",
+  },
+  {
+    name: "technic",
+    type: "crypto",
+  },
+  {
+    name: "subcancellous",
+    type: "equity",
+  },
+  {
+    name: "misauthorize",
+    type: "crypto",
+  },
+  {
+    name: "tendencious",
+    type: "crypto",
+  },
+  {
+    name: "celestite",
+    type: "equity",
+  },
+  {
+    name: "ameerate",
+    type: "crypto",
+  },
+  {
+    name: "dustless",
+    type: "equity",
+  },
+  {
+    name: "horseradish",
+    type: "equity",
+  },
+  {
+    name: "whips",
+    type: "equity",
+  },
+  {
+    name: "methylene",
+    type: "crypto",
+  },
+  {
+    name: "subpatellar",
+    type: "crypto",
+  },
+  {
+    name: "intolerableness",
+    type: "crypto",
+  },
+  {
+    name: "unterwalden",
+    type: "crypto",
+  },
+  {
+    name: "mastery",
+    type: "equity",
+  },
+  {
+    name: "giver",
+    type: "crypto",
+  },
+  {
+    name: "unsplit",
+    type: "equity",
+  },
+  {
+    name: "anticonservative",
+    type: "equity",
+  },
+  {
+    name: "feod",
+    type: "equity",
+  },
+  {
+    name: "hemoglobic",
+    type: "equity",
+  },
+  {
+    name: "ungraspable",
+    type: "crypto",
+  },
+  {
+    name: "hiver",
+    type: "crypto",
+  },
+  {
+    name: "recombine",
+    type: "crypto",
+  },
+  {
+    name: "anyplace",
+    type: "equity",
+  },
+  {
+    name: "unvindictive",
+    type: "equity",
+  },
+  {
+    name: "ontogenesis",
+    type: "crypto",
+  },
+  {
+    name: "chapleted",
+    type: "equity",
+  },
+  {
+    name: "rhizopus",
+    type: "equity",
+  },
+  {
+    name: "trochilus",
+    type: "crypto",
+  },
+  {
+    name: "quintal",
+    type: "equity",
+  },
+  {
+    name: "croupy",
+    type: "crypto",
+  },
+];
+listOfInternalWallets = [
+  {
+    name: "calxes",
+    type: "equity",
+  },
+];
+let browsExt = [...listOfExternalWallets];
+let browsInt = [...listOfInternalWallets];
+
 const wrapper = document.querySelector(".selector-container");
+const fieldset = document.querySelector(".selector-fieldset");
 const input = document.querySelector(".wallet-selector");
 const ulBox = document.querySelector(".ul-box");
 const personal = document.querySelector(".personal");
 const external = document.querySelector(".external");
 
 // Functions
-function createList(wrapper1,wrapper2,list1,list2){
-  
-  if(list1){
+function createList(wrapper1, wrapper2, list1, list2) {
+  //Clean previous list
+  wrapper1.textContent = "";
+  wrapper2.textContent = "";
+  wrapper1.style.display = "none";
+  wrapper2.style.display = "none";
+  if (list1.length >0) {
     // Show wallet list in the focus tab
     wrapper1.style.display = "block";
-
+    //Create text element
+    const par = document.createElement("p");
+    par.textContent = "External Wallets";
+    wrapper1.appendChild(par);
     // Create the wallet elements
-    list1.forEach(element =>{
+    list1.forEach((element) => {
       const li = document.createElement("li");
       li.innerHTML = element.name;
-      if(element.type == "crypto"){
+      if (element.type == "crypto") {
         const logo = document.createElement("img");
         logo.src = "res/crypt.svg";
         logo.alt = "Crypto Wallet Logo";
         li.appendChild(logo);
-      }else{
+      } else {
         const logo = document.createElement("img");
         logo.src = "res/dollar.svg";
         logo.alt = "Equity Wallet Logo";
@@ -234,30 +252,33 @@ function createList(wrapper1,wrapper2,list1,list2){
       wrapper1.appendChild(li);
 
       //append an event to each element
-      li.addEventListener("click", (event)=>{
+      li.addEventListener("click", (event) => {
         const value = event.target.innerText;
         input.value = value;
         input.innerText = value;
-      })
-    })
-  }else{
-    wrapper1.style.display ="none";
+        fieldset.style.background = "#ECECEC";
+        input.style.background = "#ECECEC";
+      });
+    });
   }
 
-  if(list2){
+  if (list2.length > 0) {
     // Show wallet list in the focus tab
     wrapper2.style.display = "block";
-
+    //Create text element
+    const par = document.createElement("p");
+    par.textContent = "Personal Wallets";
+    wrapper2.appendChild(par);
     // Create the wallet elements
-    list2.forEach(element =>{
+    list2.forEach((element) => {
       const li = document.createElement("li");
       li.innerHTML = element.name;
-      if(element.type == "crypto"){
+      if (element.type == "crypto") {
         const logo = document.createElement("img");
         logo.src = "res/crypt.svg";
         logo.alt = "Crypto Wallet Logo";
         li.appendChild(logo);
-      }else{
+      } else {
         const logo = document.createElement("img");
         logo.src = "res/dollar.svg";
         logo.alt = "Equity Wallet Logo";
@@ -266,35 +287,75 @@ function createList(wrapper1,wrapper2,list1,list2){
       wrapper2.appendChild(li);
 
       //append an event to each element
-      li.addEventListener("click", (event)=>{
+      li.addEventListener("click", (event) => {
         const value = event.target.innerText;
         input.value = value;
         input.innerText = value;
-      })
-    })
-  }else{
-    wrapper2.style.display ="none";
+        fieldset.style.background = "#ECECEC";
+        input.style.background = "#ECECEC";
+      });
+    });
+  }
+  if (!(list1.length > 0 || list2.length> 0)) {
+    wrapper1.style.display = "block";
+    const err = document.createElement("p");
+    err.textContent = "No results";
+    err.style.color = "#848484";
+    wrapper1.appendChild(err);
   }
 }
 
-//Event listeners
-input.addEventListener("focus", ()=>{
-  ulBox.style.display = "block";
-  ulBox.scrollTo(0,0);
-});
-input.addEventListener("keyup", (event) =>{
-  if(event.keyCode == 13){
-    ulBox.style.display = "none";
+function browse(){
+  console.log(browsExt,browsInt);
+  if(input.value == ""){
+    createList(external, personal, listOfExternalWallets, listOfInternalWallets);
+  }else{
+    browsExt ="";
+    browsInt = "";
+    browsExt = listOfExternalWallets.filter((value) =>{
+      const compValue = value.name.toLowerCase();
+      if(compValue.includes(input.value.toLowerCase())){
+        return true
+      }else{
+        return "";
+      }
+    })
+    browsInt = listOfInternalWallets.filter((value) =>{
+      const compValue = value.name.toLowerCase();
+      if(compValue.includes(input.value.toLowerCase())){
+        return true
+      }
+      else{
+        return false;
+      }
+    })
+    createList(external, personal, browsExt, browsInt);
   }
-})
-window.addEventListener("click", (event) =>{
-  if(event.target != input && event.target != ulBox){
-    ulBox.style.display = "none";
-  }
-})
+}
 
-//Runtime
-createList(external,personal,listOfExternalWallets,listOfInternalWallets);
+
+//Event listeners
+input.addEventListener("focus", () => {
+  browse();
+  ulBox.style.display = "block";
+  ulBox.scrollTo(0, 0);
+  fieldset.style.background = "#FFFFFF";
+  input.style.background = "#FFFFFF";
+});
+
+input.addEventListener("input", browse);
+
+input.addEventListener("keyup", (event) => {
+  if (event.keyCode == 13) {
+    ulBox.style.display = "none";
+  }
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target != input && event.target != ulBox) {
+    ulBox.style.display = "none";
+  }
+});
 
 
 
